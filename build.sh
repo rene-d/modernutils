da9d64994625f4443d9f0fef50cdeef4b71709f8
rene-d <rene-d@users.noreply.github.com>
2022-02-15 00:39:08 +0100

    gh action

--- build.sh
+++ build.sh
@@ -3,19 +3,61 @@
 set -euo pipefail
 
 builder=centos7
+dockerfile=Dockerfile
+imagename=rene2/modernutils
 
-if [[ ${1-} == -t ]]; then
+opt_test=
+opt_buildx=
+
+for i ; do
+    case $i in
+        -t|--test)
+            opt_test=1
+            ;;
+       figlet)
+            dockerfile=Dockerfile-figlet
+            imagename=rene2/figlet
+            ;;
+        -x)
+            opt_buildx=1
+            ;;
+    esac
+done
+
+if [[ $opt_test ]]; then
     run_test() {
         echo -e "Running checks on \033[93m$1\033[0m"
-        docker build --target test --build-arg RANDOM=${RANDOM} --build-arg BASETEST=$1 --build-arg BUILDER=${builder} --progress plain --tag modernutilstests . 2>&1 | grep '@ ' 
-        docker image rm modernutilstests 2>>/dev/null >/dev/null || true
+        docker build --target test \
+                     --build-arg RANDOM=${RANDOM} \
+                     --build-arg BASETEST=$1 \
+                     --build-arg BUILDER=${builder} \
+                     --progress plain \
+                     --tag testimage \
+                     --file ${dockerfile} \
+                     . 2>&1 | grep '@ '
+        docker image rm testimage 2>&1 >/dev/null || true
         echo
     }
     run_test centos:7
     run_test centos:8
     run_test debian:buster
     run_test debian:bullseye
+    run_test ubuntu:focal
     exit 0
 fi
 
-docker build --build-arg BUILDER=${builder} --tag rene2/modernutils .
+if [[ $opt_buildx ]]; then
+    # docker buildx rm buildx-local 2>&1 >/dev/null || true
+    docker buildx create --use --name buildx-local || true
+    docker buildx build --pull \
+                        --platform linux/amd64,linux/arm64 \
+                        --build-arg BUILDER=${builder} \
+                        --tag ${imagename} \
+                        --file ${dockerfile} \
+                        --push \
+                        .
+    # docker buildx rm buildx-local
+    docker manifest inspect ${imagename} | jq -r '.manifests[] | (.platform.architecture + " " + .digest)'
+else
+    docker build --build-arg BUILDER=${builder} --tag ${imagename} --file ${dockerfile} .
+fi
